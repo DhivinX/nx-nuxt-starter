fc3d2a93e95c27c1c47f033707041e21efc14a02
Dominik Szaradowski <d.szaradowski@gmail.com>
2023-01-25 05:55:41 +0100

    ci(web): configure nuxt config

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,4 +1,30 @@
+import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import fullReload from 'vite-plugin-full-reload';
+
+const resolve = (p: string) => path.resolve(__dirname, p);
+const workspaceDir = (p?: string) => resolve(`../../${p}`);
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    modulesDir: [workspaceDir('node_modules')],
+    buildDir: workspaceDir('tmp/.nuxt-web'),
     srcDir: './src',
+
+    vite: {
+        plugins: [
+            tsconfigPaths({
+                projects: [workspaceDir('tsconfig.base.json')],
+            }),
+            fullReload([workspaceDir('libs/**/*')]),
+        ],
+    },
+
+    typescript: {
+        tsConfig: {
+            extends: workspaceDir('tsconfig.base.json'),
+        },
+
+        typeCheck: true,
+    },
 });
