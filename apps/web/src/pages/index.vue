2cd36e6e9df5b64f1508e7d89c876b7d45c83274
Dominik Szaradowski <d.szaradowski@gmail.com>
2023-01-25 05:57:10 +0100

    feat: add nuxt structure and shared example function

---
+++ apps/web/src/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <NuxtWelcome />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { exampleSum } from '@workspace/shared';
+
+onMounted(() => {
+    console.log(`1 + 1 = ${exampleSum(1, 1)}`);
+});
+</script>
--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,3 +1,3 @@
-export function sum(a: number, b: number): number {
+export function exampleSum(a: number, b: number): number {
     return a + b;
 }
